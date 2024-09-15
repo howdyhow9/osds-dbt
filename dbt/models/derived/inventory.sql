14d51ac1cce7b043e81a0205f2ad1e0ac2f4bc21
howdyhow9 <163223182+howdyhow9@users.noreply.github.com>
2024-09-15 00:35:53 -0400

    store

---
+++ dbt/models/derived/inventory.sql
@@ -0,0 +1,23 @@
+{{ config(
+  materialized='table',
+  file_format='delta'
+) }}
+
+SELECT
+    ItemNum,
+    ItemName,
+    Store_ID,
+    Vendor_Number,
+    Cost,
+    Price,
+    In_Stock,
+    Reorder_Level,
+    (In_Stock - Reorder_Level) AS Stock_To_Reorder_Difference,
+    Last_Sold
+{{ source('osd_store', 'inventory') }}
+WHERE
+    In_Stock < Reorder_Level -- Identifies items that need reordering
+    AND Inactive = 0         -- Filters out inactive items
+ORDER BY
+    Store_ID,
+    Stock_To_Reorder_Difference ASC; -- Orders by stock level urgency
