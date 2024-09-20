db6efbd1b7566cb547e0cfe28fd2504341bd7c95
howdyhow9 <163223182+howdyhow9@users.noreply.github.com>
2024-09-20 00:23:59 -0400

    inv

--- dbt/models/cln/invoice_analysis.sql
+++ dbt/models/cln/invoice_analysis.sql
@@ -3,9 +3,9 @@
   file_format='delta'
 ) }}
 
-select *
+select a.*
 from {{ source('raw_storedb', 'invoice_totals') }}  a
-join {{ source('raw_storedb', 'invoice_itemized') }}  a
+join {{ source('raw_storedb', 'invoice_itemized') }}  b
 on a.Invoice_Number = b.Invoice_Number limit 5;
 
 
