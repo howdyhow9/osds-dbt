edb67156f6d256ba6782b9295592aec5a25e69f3
howdyhow9 <163223182+howdyhow9@users.noreply.github.com>
2024-09-22 16:55:22 -0400

    Update invoice_analysis.sql

--- dbt/models/cln/invoice_analysis.sql
+++ dbt/models/cln/invoice_analysis.sql
@@ -3,9 +3,12 @@
   file_format='delta'
 ) }}
 
-select a.*
-from {{ source('raw_storedb', 'invoice_totals') }}  a
-join {{ source('raw_storedb', 'invoice_itemized') }}  b
-on a.Invoice_Number = b.Invoice_Number;
 
 
+select IT.Invoice_Number ,LineNum,DiffItemName,"DateTime",
+Quantity,CostPer,PricePer,origPricePer,Total_Cost,Payment_Method,
+Total_UndiscountedSale,PricePerBeforeDiscount
+from {{ source('raw_storedb', 'invoice_totals') }} IT
+left join {{ source('raw_storedb', 'invoice_itemized') }} i
+on I.Invoice_Number =IT.Invoice_Number
+WHERE IT.DateTime > date_add(current_date(), -423);
